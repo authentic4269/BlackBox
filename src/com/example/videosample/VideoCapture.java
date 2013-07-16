3cbcdce86bb2f27c509eada9dfac2ced23c3d66e
Mitchell Vogel <mjv58@cornell.edu>
2013-07-16 13:25:59 +0700

    initial

---
+++ src/com/example/videosample/VideoCapture.java
@@ -0,0 +1,102 @@
+package com.example.videosample;
+/* timed recording to incremental files */
+/* running as a service */
+/
+
+import java.io.IOException;
+
+import com.example.videosample.R;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class VideoCapture extends Activity implements OnClickListener, SurfaceHolder.Callback {
+    MediaRecorder recorder;
+    SurfaceHolder holder;
+    boolean recording = false;
+    int video_number;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_video_capture);
+        recorder = new MediaRecorder();
+        initRecorder();
+       
+
+        SurfaceView cameraView = (SurfaceView) findViewById(R.id.surface_camera);
+        holder = cameraView.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        Button startButton = (Button) findViewById(R.id.buttonstart);
+        startButton.setClickable(true);
+        startButton.setOnClickListener(this);
+    }
+
+    private void initRecorder() {
+        recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+
+        CamcorderProfile cpHigh = CamcorderProfile
+                .get(CamcorderProfile.QUALITY_HIGH);
+        recorder.setProfile(cpHigh);
+        recorder.setOutputFile("/sdcard/videocapture_example" + video_number + "10.mp4");
+        recorder.setMaxDuration(50000); // 50 seconds
+        recorder.setMaxFileSize(5000000); // Approximately 5 megabytes
+    }
+
+    private void prepareRecorder() {
+        recorder.setPreviewDisplay(holder.getSurface());
+        recorder.setVideoFrameRate(5);
+        try {
+            recorder.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            finish();
+        } catch (IOException e) {
+            e.printStackTrace();
+            finish();
+        }
+    }
+
+    public void onClick(View v) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+
+            // Let's initRecorder so we can record again
+        } else {
+            recording = true;
+            recorder.start();
+        }
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        prepareRecorder();
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+            int height) {
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+        }
+        recorder.release();
+        finish();
+    }
+}
